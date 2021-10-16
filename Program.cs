abe52060932d8228aa99767200bebe126faa3725
vladc2 <vladc2@VLADC2-T580>
2021-10-16 09:05:11 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyFirstApp_Lab1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
